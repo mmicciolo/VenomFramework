e589b2211b13f6a298714c98704d1dadda7a7f04
mmicciolo <mmicciolo@wpi.edu>
2018-06-02 18:12:52 -0400

    Transpose Matrices now.

--- src/test/Test4 (VertexBuffer)/Test4VertexBuffer.cpp
+++ src/test/Test4 (VertexBuffer)/Test4VertexBuffer.cpp
@@ -1,11 +1,15 @@
 #include "Test4VertexBuffer.h"
 #include "../../graphics/buffer/VertexPosition/VertexPosition.h"
+#include "../../graphics/buffer/VertexPositionColor/VertexPositionColor.h"
 
 VF::Graphics::VertexBuffer * CreateVertexBuffer(VF::Graphics::IGraphicsDevice * graphicsDevice) {
 	VF::Graphics::IVertexType ** vertices = new VF::Graphics::IVertexType*[3];
-	vertices[0] = new VF::Graphics::VertexPosition(*(new VF::Math::Vector3(-1.0f, -1.0f, 0.0f)));
-	vertices[1] = new VF::Graphics::VertexPosition(*(new VF::Math::Vector3(0.0f, 1.0f, 0.0f)));
-	vertices[2] = new VF::Graphics::VertexPosition(*(new VF::Math::Vector3(1.0f, -1.0f, 0.0f)));
+	//vertices[0] = new VF::Graphics::VertexPosition(*(new VF::Math::Vector3(-1.0f, -1.0f, 0.0f)));
+	//vertices[1] = new VF::Graphics::VertexPosition(*(new VF::Math::Vector3(0.0f, 1.0f, 0.0f)));
+	//vertices[2] = new VF::Graphics::VertexPosition(*(new VF::Math::Vector3(1.0f, -1.0f, 0.0f)));
+	vertices[0] = new VF::Graphics::VertexPositionColor(*(new VF::Math::Vector3(-1.0f, -1.0f, 0.0f)), *(new VF::Math::Vector4(0.5f, 1.0f, 1.0f, 1.0f)));
+	vertices[1] = new VF::Graphics::VertexPositionColor(*(new VF::Math::Vector3(0.0f, 1.0f, 0.0f)), *(new VF::Math::Vector4(1.0f, 0.5f, 1.0f, 1.0f)));
+	vertices[2] = new VF::Graphics::VertexPositionColor(*(new VF::Math::Vector3(1.0f, -1.0f, 0.0f)), *(new VF::Math::Vector4(1.0f, 1.0f, 0.5f, 1.0f)));
 	VF::Graphics::VertexBuffer * vertexBuffer = new VF::Graphics::VertexBuffer(graphicsDevice, 3, vertices[0]->GetVertexDecleration(), VF::Graphics::BufferUsage::Usage::WriteOnly);
 	vertexBuffer->SetData(vertices);
 	return vertexBuffer;
@@ -29,7 +33,10 @@ void Test4VertexBuffer::Init() {
 	float ar = 640 / 480;
 	basicEffect->parameters.projection = VF::Math::perspectiveFovLH<float>(VF::Math::radians(45.0f), 640.0f, 480.0f, 0.1f, 100.f);
 	basicEffect->parameters.view = VF::Math::lookAtLH(VF::Math::Vector3(0, 0, -10), VF::Math::Vector3(0, 0, 0), VF::Math::Vector3(0, 1, 0));
-	basicEffect->parameters.world = VF::Math::translate(VF::Math::mat4(1.0f), VF::Math::Vector3(0, 0, 0));
+	basicEffect->parameters.world = VF::Math::rotate(VF::Math::Matrix4(1.0f), VF::Math::radians<float>(0), VF::Math::Vector3(0, 0, 1)) * VF::Math::translate(VF::Math::Matrix4(1.0f), VF::Math::Vector3(0, 0, 0));
+	basicEffect->parameters.projection = VF::Math::transpose(basicEffect->parameters.projection);
+	basicEffect->parameters.view = VF::Math::transpose(basicEffect->parameters.view);
+	basicEffect->parameters.world = VF::Math::transpose(basicEffect->parameters.world);
 }
 
 void Test4VertexBuffer::Update() {
